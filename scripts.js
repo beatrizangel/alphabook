f6f850ccec76defc82a2c96da86614d42827ef7b
BEATRIZ DOS ANJOS LOPES BERNARD <beatriz.albernard@senacsp.edu.br>
2021-03-02 11:47:06 -0300

    Adição dos arquivos

---
+++ scripts.js
@@ -0,0 +1,18 @@
+			var slideIndex = 0;
+			mostraSlides(slideIndex);
+
+			function acrecentaSlides(n){
+				mostraSlides(slideIndex = slideIndex + n);
+			}
+
+			function mostraSlides(n){
+			var slides = document.getElementsByClassName("slide");
+			
+			if (n>=slides.length){slideIndex=0;}
+			if (n<0){slideIndex=slides.length-1;}
+
+			for(i=0;i<slides.length;i++){
+				slides[i].style.display = "none";
+			}	
+			slides[slideIndex].style.display = "block";
+		}
